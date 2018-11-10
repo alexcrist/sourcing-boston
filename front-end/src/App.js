c2518b9d027c7afaf3dd973f01b749ea32567967
David Stein <david.f.stein@gmail.com>
2018-11-10 13:29:43 -0500

    homepage

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -4,7 +4,17 @@ import './App.css';
 class App extends Component {
   render() {
     return (
-      <div></div>
+      <div className="home">
+        <div className="container home-container">
+          <div className="form-cont">
+            <div className="zip-form">
+              <label className="zip-label" for="zipcode">Enter Your Zip Code</label>
+              <input type="text" placeholder="zipcode" class="form-control" id="zipcode"></input>
+            </div>
+            <button className="btn btn-primary zip-sub">Submit</button>
+          </div>
+        </div>
+      </div>
     );
   }
 }
