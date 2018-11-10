034734747fb00390d0dd56446de905c5dd20dabd
alexcrist <alexecrist@gmail.com>
2018-11-10 16:20:07 -0500

    Added util stuff

--- util/data-transformer.py
+++ util/data-transformer.py
@@ -3,6 +3,27 @@ import re
 data = open("data.txt", "r")
 string = data.read()
 
+
+keys = {
+  0: food_pantires,
+  105: SNAP application
+}
+
+food_banks = {
+  "FOOD PANTRIES": [],
+  "MEALS SITES": [],
+  "SNAP APPLICATION ASSISTANCE": [],
+  "FARMERS MARKETS ACCEPTING SNAP/HIP": [],
+  "SENIOR DINING STIES": [],
+  "AFFORDABLE FRUIT & VEGETABLE SOURCES": []
+}
+
+while True:
+
+  for 
+  
+
+
 t = re.split('\d+\.', string)
 for line in t:
   print(line)
