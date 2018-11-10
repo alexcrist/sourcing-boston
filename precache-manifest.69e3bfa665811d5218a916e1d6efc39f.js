ad09d8bdf9f25cb7bd58f82a4ea46c427a3bee9d
alexcrist <alexecrist@gmail.com>
2018-11-10 12:46:57 -0500

    Updates

---
+++ precache-manifest.69e3bfa665811d5218a916e1d6efc39f.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e0690500552e815d03e7",
+    "url": "/sourcing-boston/static/js/runtime~main.e0690500.js"
+  },
+  {
+    "revision": "2f4d5e1788c9efacc124",
+    "url": "/sourcing-boston/static/js/main.2f4d5e17.chunk.js"
+  },
+  {
+    "revision": "0cdd6f184db929cffcf5",
+    "url": "/sourcing-boston/static/js/1.0cdd6f18.chunk.js"
+  },
+  {
+    "revision": "2f4d5e1788c9efacc124",
+    "url": "/sourcing-boston/static/css/main.f04b8fe7.chunk.css"
+  },
+  {
+    "revision": "70a19a13bd9ebb9c69bc814634f0e774",
+    "url": "/sourcing-boston/index.html"
+  }
+];
