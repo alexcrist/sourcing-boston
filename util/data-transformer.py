b6201db5edbc948a0d2915ee5fc7ef11de7f5e95
David Stein <david.f.stein@gmail.com>
2018-11-10 19:46:31 -0500

    data so clean

--- util/data-transformer.py
+++ util/data-transformer.py
@@ -1,6 +1,6 @@
 import re
 
-data = open("data.txt", "r")
+data = open("demofile.txt", "r")
 string = data.read()
 
 
@@ -9,10 +9,17 @@ food_banks = {
   "MEALS SITES": [],
   "SNAP APPLICATION ASSISTANCE": [],
   "FARMERS MARKETS ACCEPTING SNAP/HIP": [],
-  "SENIOR DINING STIES": [],
+  "SENIOR DINING ASSISTANCE": [],
   "AFFORDABLE FRUIT & VEGETABLE SOURCES": []
 }
 
-t = re.split('\d+\.', string)
-for line in t:
-  print(line)
+x = re.split("HELLO", string)
+
+food_banks["FOOD PANTRIES"] = re.split("FOOD PANTRIES", x[0])[1]
+food_banks["MEALS SITES"] = re.split("MEALS SITES", x[1])[1]
+food_banks["SNAP APPLICATION ASSISTANCE"] = re.split("SNAP APPLICATION ASSISTANCE", x[2])[1]
+food_banks["FARMERS MARKETS ACCEPTING SNAP/HIP"] = re.split("FARMERS MARKETS ACCEPTING SNAP/HIP", x[3])[1]
+food_banks["SENIOR DINING ASSISTANCE"] = re.split("SENIOR DINING ASSISTANCE", x[4])[1]
+food_banks["AFFORDABLE FRUIT & VEGETABLE SOURCES"] = re.split("AFFORDABLE FRUIT & VEGETABLE SOURCES", x[5])[1]
+
+
