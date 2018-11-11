4063930f16fb3520784a56d00af014c44169ffa2
David Stein <david.f.stein@gmail.com>
2018-11-11 12:15:37 -0500

    style

--- front-end/src/components/day/Day.js
+++ front-end/src/components/day/Day.js
@@ -9,24 +9,29 @@ class Day extends Component {
     render() {
         let { day, date, sources } = this.props.day
         return (
-            <div>
-                <h4>{day} | {date}</h4>
-                {sources.map(source => {
-                    let { name, address, phone, availability, type } = source.source;
-                    console.log(name, address, phone, availability, type)
-
-                    console.log()
-                    return (
-                        <div>
-                            <b><p>{name}</p></b>
-                            <p>{type}</p>
-                            <p>{address}</p>
-                            <p>{phone}</p>
-                            <p>Today's Hours: {availability[day.toLowerCase()].join("-")}</p>
-                        </div>
-                    );
-                })}
+            <div className="card">
+                <div clasName="card-body" style={{padding: "10px", backgroundColor: "#f7f7f9", boxShadow: "5px 10px grey"}}>
+                    <div>
+                        <h4>{day} | {date}</h4>
+                        {console.log(sources)}
+                        {sources.map(source => {
+                            let { name, address, phone, availability, type } = source.source;
+                            console.log(name, address, phone, availability, type)
+                            console.log()
+                            return (
+                                <div>
+                                    <b><p>{name}</p></b>
+                                    <p>{type}</p>
+                                    <p>{address}</p>
+                                    <p>{phone}</p>
+                                    <p>Today's Hours: {availability[day.toLowerCase()].join("-")}</p>
+                                </div>
+                            );
+                        })}
+                    </div>
+                </div>
             </div>
+            
         )
     }
 }
